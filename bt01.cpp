498ed012709fc288efe6a3b4a1e05358aa51d948
khanhtrngia <160708055+khanhtrngia@users.noreply.github.com>
2024-03-01 16:35:46 +0700

    Add files via upload

---
+++ bt01.cpp
@@ -0,0 +1,91 @@
+////13
+//#include <bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+//    double x, y;
+//    cin >> x >> y;
+//    cout << sqrt( x*x + y*y );
+//    return 0;
+//}
+
+
+
+////15
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    string s[11]={"a", "b"};
+//    for(int i=2; i<11; i++)
+//    {
+//        s[i]=s[i-1]+s[i-2];
+//    }
+//    for(int i=0; i<11; i++)
+//    {
+//        cout << s[i] << ' ';
+//    }
+//    return 0;
+//}
+
+
+////16
+//#include <bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+//    int a, b, c;
+//    cin >> a >> b >> c;
+//    cout << ((a==b && b==c)?"yes" : "false");
+//}
+
+
+////17
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//int main()
+//{
+//    int i; cin >> i;
+//    cout << ( ((i%4==0) && (i%100!=0))?"true" : "false");
+//}
+
+
+////18
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    double x1, x2, y1, y2;
+//    cin >> x1 >> y1 >> x2 >> y2;
+//    cout << sqrt(pow(x1-x2, 2) + pow(y1-y2, 2));
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
