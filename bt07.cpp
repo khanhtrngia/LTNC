89761ecc7bba4c4ab8e1e47d25adc32b3ed49fe9
khanhtrngia <khanhicloud05@icloud.com>
2024-03-27 21:41:40 +0700

    khanh

---
+++ bt07.cpp
@@ -0,0 +1,58 @@
+/*
+7d
+8a
+9c
+10b
+11cO
+12b
+13d
+14d
+15b
+16b
+17a
+18a
+19b
+20c
+21b
+22c
+23c
+24a
+25a
+26d
+*/
+
+
+////5
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//bool check_songuyento(int n)
+//{
+//    for(int i=2; i<=n/2; i++)
+//    {
+//        if(n%i==0) return 0;
+//    }
+//    return 1;
+//}
+//
+//void print_songuyento(int n)
+//{
+//    for(int i=1; i<n; i++)
+//    {
+//        if(check_songuyento(i)==1) cout << i << ' ';
+//    }
+//}
+//int main()
+//{
+//    int n; cin >> n;
+//    cout << boolalpha << check_songuyento(n) << endl;
+//    print_songuyento(n);
+//}
+
+
+
+
+
+
+
--- tesst.cpp
+++ tesst.cpp
@@ -1,49 +1,15 @@
 #include <iostream>
-#include <vector>
-
-using namespace std;
-
-void printMagicSquare(vector<vector<int>> &magicSquare) {
-    for (const auto &row : magicSquare) {
-        for (int num : row) {
-            cout << num << " ";
-        }
-        cout << endl;
-    }
+int foo(int y);
+int foo(int x)
+{
+  return x+1;
 }
+int main(int argc, char** argv)
+{
+  int x = 3;
+  int y = 6;
 
-void generateMagicSquare(int N) {
-    if (N % 2 == 0) {
-        cout << "Vui lòng nhập một số lẻ." << endl;
-        return;
-    }
+  std::cout << foo(x) << std::endl;
 
-    vector<vector<int>> magicSquare(N, vector<int>(N, 0));
-
-    int num = 1;
-    int i = 0, j = N / 2;
-
-    while (num <= N * N) {
-        magicSquare[i][j] = num;
-        num++;
-        int newi = (i - 1 + N) % N;
-        int newj = (j + 1) % N;
-        if (magicSquare[newi][newj]) {
-            i++;
-        } else {
-            i = newi;
-            j = newj;
-        }
-    }
-
-    cout << "Ma trận kì ảo có kích thước " << N << " x " << N << " là:" << endl;
-    printMagicSquare(magicSquare);
-}
-
-int main() {
-    int N;
-    cout << "Nhập vào một số nguyên dương lẻ N: ";
-    cin >> N;
-    generateMagicSquare(N);
-    return 0;
+  return 0;
 }
