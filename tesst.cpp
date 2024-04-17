72290ca315492db443dd39ebc47a6c369428a901
khanhtrngia <khanhicloud05@icloud.com>
2024-04-17 14:26:53 +0700

    khanh

--- tesst.cpp
+++ tesst.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main()
 {
-    int *p = new int[3]; p[0]=1; p[1]=2; p[2]=4; int a=&p[2]-&p[1];
-    cout << a;
+    int n; cin >> n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+    int minnum=INT_MAX;
+    for(int i=0; i<n; i++)
+    {
+        for(int y=0; y<n; y++)
+        {
+            if(y==i) continue;
+            int d=abs(a[y]-a[i]);
+            minnum=min(d, minnum);
+        }
+    }
+    cout << minnum;
 }
