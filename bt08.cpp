ebb5ee3f8f373d8426d56b520ad311a90c9f5445
khanhtrngia <khanhicloud05@icloud.com>
2024-04-21 11:24:27 +0700

    khanh

--- bt08.cpp
+++ bt08.cpp
@@ -61,9 +61,72 @@ void pad_right(char s[], int n)
     s[n]=NULL;
 }
 
+void pad_left(char s[], int n)
+{
+    int l= strlenn(s);
+    if(l<n)
+    {
+        reversee(s);
+        for(int i=l; i<n; i++)
+        {
+            s[i]=' ';
+        }
+    }
+    s[n]=NULL;
+    reversee(s);
+}
+
+bool is_palindromee(char s[])
+{
+    bool b=1;
+    int l = strlenn(s);
+    for(int i=0; i<l/2; i++)
+    {
+        if(s[i]!=s[l-1-i])
+        {
+            b=0;
+        }
+    }
+    return b;
+}
+
+void trim_left(char s[])
+{
+    int l=strlenn(s);
+    int dem=0;
+    while(s[dem]==' ')
+    {
+        dem++;
+    }
+
+    for(int i=0; i<(l-dem); i++)
+    {
+        s[i]=s[i+dem];
+    }
+    s[l-dem]=NULL;
+}
+
+trim_right(char s[])
+{
+    reversee(s);
+    int l=strlenn(s);
+    int dem=0;
+    while(s[dem]==' ')
+    {
+        dem++;
+    }
+
+    for(int i=0; i<(l-dem); i++)
+    {
+        s[i]=s[i+dem];
+    }
+    s[l-dem]=NULL;
+    reversee(s);
+}
+
 int main()
 {
-    char s[]="khanh";
-    pad_right(s, 10);
-    cout << strlen(s);
+    char s[]="khanh  ";
+    trim_right(s);
+    cout << s << ' '<< strlenn(s);
 }
