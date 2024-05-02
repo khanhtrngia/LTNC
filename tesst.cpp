c9fbbaeb07fa84ec330393bf67c43ef7f3282b5c
khanhtrngia <khanhicloud05@icloud.com>
2024-05-02 23:57:43 +0700

    khanh

--- tesst.cpp
+++ tesst.cpp
@@ -2,105 +2,84 @@
 
 using namespace std;
 
-int strlenn(const char s[])
-{
-    int ans=0;
-    while(*(s+ans)!=NULL)
-    {
-        ans++;
-    }
-    return ans;
-}
-
-string gan(const char s[])
-{
-    int l=strlenn(s);
-    char ans[l];
-    for(int i=0; s[i]!=NULL; i++)
-    {
-        ans[i]=s[i];
-    }
-    return ans;
-}
-
-void reversee(const char a[])
-{
-    int l=strlenn(a);
-    string s= gan(a);
-    for(int i=0; i<l/2; i++)
-    {
-        char temp=s[i];
-        s[i]=s[l-1-i];
-        s[l-1-i]=temp;
-    }
-    cout << s << endl;
-}
-
-void delete_char(const char a[], char c)
-{
-    int l=strlenn(a);
-    int j=0;
-    char s[l];
-    for(int i=0; i<l; i++)
-    {
-        if(a[i]!=c)
-        {
-            s[j++]=a[i];
-        }
-    }
-    cout << s << endl;
-}
-
-void pad_right(const char a[], int n)
-{
-    int l=strlenn(a);
-    if(l<n)
-    {
-        char s[n];
-        for(int i=0; i<n; i++)
-        {
-            if(i<l)
-            {
-                s[i]=a[i];
-            }
-            if(i>=l)
-            {
-                s[i]=' ';
-            }
-        }
-        s[n]=NULL;
-        cout << s << ' ' << strlenn(s) << endl;
-    }
-}
-
-void pad_left(const char a[], int n)
-{
-    int l=strlenn(a);
-    if(l<n)
-    {
-        char s[n];
-        int j=0;
-        for(int i=0; i<n; i++)
-        {
-            if(i<n-l)
-            {
-                s[i]=' ';
-            }
-            if(i>=n-l)
-            {
-                s[i]=a[j++];
-            }
-        }
-        s[n]=NULL;
-        cout << s << ' ' << strlenn(s) << endl;
-    }
-}
-
-
-
 int main()
 {
-    const char s[]="khanh";
-    string a=s;
-    cout << a;
+    int n; cin >> n;
+    int a[n+1][n+1];
+    for(int i=0; i<n+1;i++)
+    {
+        for(int y=0; y<n+1; y++)
+        {
+            a[i][y]=0;
+        }
+    }
+
+    for(int i=1; i<=n; i++)
+    {
+        int x, y; cin >> x >> y;
+        a[x][y]++;
+    }
+    int check=0;
+
+
+    for(int i=1; i<n+1; i++)
+    {
+        int temp=0;
+        for(int y=1; y<n+1; y++)
+        {
+            if(a[i][y]!=0) temp++;
+        }
+        if(temp>1) check++;
+    }
+    for(int y=1; y<n+1; y++)
+    {
+        int temp=0;
+        for(int i=1; i<n+1; i++)
+        {
+            if(a[i][y]!=0) temp++;
+        }
+        if(temp>1) check++;
+    }
+    for(int y=1; y<n+1;y++)
+    {
+        int i=0;
+        int j=y;
+        int temp=0;
+        for(int x=1; x<=y; x++)
+        {
+            if(a[i][j]!=0) temp++;
+            i++;
+            j--;
+        }
+        if(temp>1) check++;
+    }
+    for(int y=1; y<n+1;y++)
+    {
+        int i=n;
+        int j=y;
+        int temp=0;
+        for(int x=1; x<=y; x++)
+        {
+            if(a[i][j]!=0) temp++;
+            i--;
+            j--;
+        }
+        if(temp>1) check++;
+    }
+    if(check>0) cout << "YES";
+    if(check==0) cout << "NO";
 }
+
+
+
+//. 1 . .
+//. . . 1
+//1 . . .
+//. . 1 .
+
+
+
+
+
+
+
